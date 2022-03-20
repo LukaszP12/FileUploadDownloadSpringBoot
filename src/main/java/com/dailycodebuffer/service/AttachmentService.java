cf20ba7f51e1329987e1165486db368e8ee1d373
Lukasz Piwowarski <lukaszp4@onet.eu>
2022-03-20 21:27:42 +0100

    init commit spring-boot-file-upload

---
+++ src/main/java/com/dailycodebuffer/service/AttachmentService.java
@@ -0,0 +1,12 @@
+package com.dailycodebuffer.service;
+
+import com.dailycodebuffer.entity.Attachment;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AttachmentService {
+
+    Attachment saveAttachment(MultipartFile file) throws Exception;
+
+    Attachment getAttachment(String fileId) throws Exception;
+
+}
